2833b50ace81104d7eef7ee4bbd5d76bfc3220fd
shivam.goyal <shivam.goyal@squareyards.co.in>
2024-07-06 15:10:01 +0530

    changes

--- bookhub/src/modules/Home.jsx
+++ bookhub/src/modules/Home.jsx
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero'
 import Products from '../components/Products'
 
 const Home = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/b/YEP3');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
-        <Hero/>
-        <h2 className='text-6xl font-bold text-center mt-20'>Products</h2>
-        <Products/>
+        {/* <Hero/> */}
+        <h2 className='text-6xl font-bold text-center mt-20'>Books</h2>
+        <Products data={data}/>
     </div>
   )
 }
